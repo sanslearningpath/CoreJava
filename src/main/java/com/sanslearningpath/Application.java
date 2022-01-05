43a2aef4322aba2dd60e30693a4b612dd589a619
Neenu Sara Sunny <neenu.sunny@edstem.com>
2022-01-06 02:05:24 +0530

    bank account problem

---
+++ src/main/java/com/sanslearningpath/Application.java
@@ -0,0 +1,12 @@
+package com.sanslearningpath;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+
+	}
+
+}
